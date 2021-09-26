6ce1aab267abe3d5d697c03e6be04a31675b4c10
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-26 15:44:59 +0300

    added imports

---
+++ app/main/views.py
@@ -0,0 +1,7 @@
+from flask import render_template, request, redirect, url_for, abort, flash
+from . import main
+from flask_login import login_required, current_user
+from ..models import User, Post, Comment
+from .forms import UpdateProfile, PostForm, CommentForm
+from .. import db, photos
+from ..request import get_quotes
