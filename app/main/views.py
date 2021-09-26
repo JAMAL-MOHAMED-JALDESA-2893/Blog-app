613347515be42175a8de09fe6a6427baf1cb0183
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-26 15:43:41 +0300

    removed dummy code

--- app/main/views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, request, redirect, url_for, abort, flash
-from ..requests  import  get_quote
-from . import main
-
-
-@main.route('/')
-def index():
-    quote = get_quote()
-    return render_template('index.html', quote = quote)
